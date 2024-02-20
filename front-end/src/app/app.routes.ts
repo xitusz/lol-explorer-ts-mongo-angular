d9a2bbe2a5b6ef7ad50683e1a60a3c002a8533ad
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 16:48:51 -0300

    feat(): champion, region, login, register and profile in app.routes.ts in the frontend

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ChampionComponent } from './components/champion/champion.component';
+import { RegionComponent } from './components/region/region.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'champion', component: ChampionComponent },
+  { path: 'region', component: RegionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+];
