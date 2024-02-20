f7a1a73270604d66a08b926339ae728cf258c131
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 17:47:45 -0300

    feat(components): championsImg and regionsImg properties in home in the frontend

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  championsImg: string = '../../../assets/images/champions.png';
+  regionsImg: string = '../../../assets/images/regions.png';
+}
