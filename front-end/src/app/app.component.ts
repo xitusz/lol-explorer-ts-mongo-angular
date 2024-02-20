d64bb07deb7d0f62325523b5fa78ea8d831fcd1b
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 12:55:40 -0300

    chore(): remove footer component in app.component in the frontend

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
