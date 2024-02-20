5561ec1e76c19d040682587a48fc1a7a97753b85
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 16:46:54 -0300

    feat(components): profile-edit in the frontend

---
+++ front-end/src/app/components/profile-edit/profile-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-profile-edit',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './profile-edit.component.html',
+  styleUrl: './profile-edit.component.css'
+})
+export class ProfileEditComponent {
+
+}
