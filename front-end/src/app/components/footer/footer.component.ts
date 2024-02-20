dc8c3f13f56504704eb25775aa13528df4218a31
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 12:56:21 -0300

    feat(components): input property className and method scrollToTop in footer in the frontend

--- front-end/src/app/components/footer/footer.component.ts
+++ front-end/src/app/components/footer/footer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  @Input() className: string = '';
 
+  constructor() {}
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
 }
