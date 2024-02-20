210187f1ee1eac302f814da221d80f73656ef474
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 16:47:34 -0300

    feat(components): region-details in the frontend

---
+++ front-end/src/app/components/region-details/region-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-region-details',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './region-details.component.html',
+  styleUrl: './region-details.component.css'
+})
+export class RegionDetailsComponent {
+
+}
