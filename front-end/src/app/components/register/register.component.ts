d0a74f738ca8833840bd26690a7afeaf34c24086
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 16:46:33 -0300

    feat(components): register in the frontend

---
+++ front-end/src/app/components/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
