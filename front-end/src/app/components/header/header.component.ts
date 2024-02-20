1232686ee73bbadedfe539bf7a54e0a819fe6555
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-20 16:52:35 -0300

    feat(components): method ngOnInit, handleExiting and toggleCollapsed in header in the frontend

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  loggedUser: boolean = false;
+  collapsed: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      const isLoggedIn = localStorage.getItem('isLoggedIn');
+      this.loggedUser = isLoggedIn ? JSON.parse(isLoggedIn) : false;
+    }
+  }
+
+  handleExiting(): void {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.removeItem('token');
+      localStorage.removeItem('isLoggedIn');
+    }
+    this.loggedUser = false;
+    this.router.navigate(['/login']);
+  }
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
 }
