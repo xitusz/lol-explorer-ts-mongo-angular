db931a26aa627508d2ec770001663091c94ed053
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-22 15:06:35 -0300

    feat(): championDetails and regionDetails in app.routes.ts in the frontend

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ChampionComponent } from './components/champion/champion.component';
+import { ChampionDetailsComponent } from './components/champion-details/champion-details.component';
 import { RegionComponent } from './components/region/region.component';
+import { RegionDetailsComponent } from './components/region-details/region-details.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -9,7 +11,9 @@ import { ProfileComponent } from './components/profile/profile.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'champion', component: ChampionComponent },
+  { path: 'champion/:championName', component: ChampionDetailsComponent },
   { path: 'region', component: RegionComponent },
+  { path: 'region/:regionName', component: RegionDetailsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
