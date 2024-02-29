fd95a0925fa4f35ec691f93124af06eb9728e3dc
G-Alves1 <2kgabrielalves@gmail.com>
2024-02-29 20:22:13 -0300

    feat(components): method ngOnInit, fetchRegionDetails and navigateToChampion in region-details in the frontend

--- front-end/src/app/components/region-details/region-details.component.ts
+++ front-end/src/app/components/region-details/region-details.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { IRegion } from '../../interfaces/region.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RegionDetailsService } from '../../services/region-details.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-region-details',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './region-details.component.html',
-  styleUrl: './region-details.component.css'
+  styleUrl: './region-details.component.css',
 })
-export class RegionDetailsComponent {
+export class RegionDetailsComponent implements OnInit {
+  regionDetails: IRegion = {
+    name: '',
+    nameBR: '',
+    img: '',
+    regionVideo: '',
+    icon: '',
+    description: '',
+    champions: [],
+  };
+  loading: boolean = true;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private regionDetailsService: RegionDetailsService
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchRegionDetails();
+  }
+
+  fetchRegionDetails(): void {
+    const regionName: string | null = this.route.snapshot.params['regionName'];
+    if (!regionName) return;
+
+    this.regionDetailsService.getRegionByName(regionName).subscribe((region) => {
+      if (!region) {
+        this.loading = true;
+        return;
+      }
+      this.regionDetails = region;
+      this.loading = false;
+    });
+  }
+
+  navigateToChampion(championId: string): void {
+    this.router.navigate([`champion/${championId}`]);
+  }
 }
