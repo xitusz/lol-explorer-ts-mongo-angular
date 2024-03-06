a29e14a033569b32627a3c5e1bc52740de781175
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-06 16:39:05 -0300

    feat(guards): auth in the frontend

---
+++ front-end/src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
+  let isLoggedIn;
+  let token;
+
+  if (typeof localStorage !== 'undefined') {
+    isLoggedIn = localStorage.getItem('isLoggedIn');
+    token = localStorage.getItem('token');
+  }
+
+  if (isLoggedIn && token) {
+    return true;
+  } else {
+    router.navigateByUrl('');
+    return false;
+  }
+};
