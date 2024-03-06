a048ce9f9d0f87cc564e147d155e0a3f91ef305f
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-06 16:40:35 -0300

    feat(): auth and not-auth guards in profile, profile/edit, login and register in app.routes.ts in the frontend

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { RegionDetailsComponent } from './components/region-details/region-detai
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileEditComponent } from './components/profile-edit/profile-edit.component';
+import { authGuard } from './guards/auth.guard';
+import { notAuthGuard } from './guards/not-auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,7 +17,8 @@ export const routes: Routes = [
   { path: 'champion/:championName', component: ChampionDetailsComponent },
   { path: 'region', component: RegionComponent },
   { path: 'region/:regionName', component: RegionDetailsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent, canActivate: [notAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [notAuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+  { path: 'profile/edit', component: ProfileEditComponent, canActivate: [authGuard] },
 ];
