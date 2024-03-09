2e894742185ab3e8ef4212adebc0c7650f02cc96
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-09 18:09:57 -0300

    chore(components): name, email, password and confirmPassword validation in register in the frontend

--- front-end/src/app/components/register/register.component.ts
+++ front-end/src/app/components/register/register.component.ts
@@ -24,6 +24,21 @@ export class RegisterComponent {
   siteKey: string = environment.RECAPTCHA_SITE_KEY;
   isBrowser: boolean;
 
+  readonly ERRORS = {
+    NAME_REQUIRED: 'Nome é obrigatório.',
+    NAME_ERROR: 'O nome deve ter pelo menos 2 caracteres.',
+    EMAIL_REQUIRED: 'Email é obrigatório.',
+    EMAIL_ERROR: 'Insira um email válido.',
+    EMAIL_ALREADY_REGISTERED: 'Este email já está registrado.',
+    PASSWORD_REQUIRED: 'Senha é obrigatória.',
+    PASSWORD_ERROR: 'A senha deve ter de 6 a 12 caracteres.',
+    CONFIRM_PASSWORD_REQUIRED: 'Confirmar senha é obrigatório.',
+    CONFIRM_PASSWORD_ERROR: 'As senhas não são iguais.',
+    RECAPTCHA_ERROR: 'Captcha inválido.',
+    TERMS_ERROR: 'Você deve concordar com os Termos e Condições.',
+    REGISTRATION_ERROR: 'Erro no registro.',
+  };
+
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
@@ -39,7 +54,7 @@ export class RegisterComponent {
         password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
         confirmPassword: ['', [Validators.required]],
         recaptcha: [null, [Validators.required]],
-        agreedToTerms: [false, [Validators.required]],
+        agreedToTerms: [null, [Validators.required]],
       },
       {
         validators: [Validation.matchPassword('password', 'confirmPassword')],
@@ -48,23 +63,54 @@ export class RegisterComponent {
   }
 
   onSubmit(): void {
-    const { name, email, password, recaptcha, agreedToTerms } = this.registrationForm.value;
+    const { name, email, password } = this.registrationForm.value;
+    const {
+      NAME_REQUIRED,
+      NAME_ERROR,
+      EMAIL_REQUIRED,
+      EMAIL_ERROR,
+      PASSWORD_REQUIRED,
+      PASSWORD_ERROR,
+      CONFIRM_PASSWORD_REQUIRED,
+      CONFIRM_PASSWORD_ERROR,
+      RECAPTCHA_ERROR,
+      TERMS_ERROR,
+    } = this.ERRORS;
 
-    if (this.registrationForm.invalid) {
-      this.error = 'Preencha todos os campos.';
-    } else if (!recaptcha) {
-      this.error = 'Captcha inválido';
-    } else if (!agreedToTerms) {
-      this.error = 'Você deve concordar com os Termos e Condições.';
-    } else {
-      this.checkRegisterUser(name, email, password /*recaptcha*/);
+    switch (true) {
+      case this.checkErrorAndSetMessage('name', 'required', NAME_REQUIRED):
+      case this.checkErrorAndSetMessage('name', 'minlength', NAME_ERROR):
+      case this.checkErrorAndSetMessage('email', 'required', EMAIL_REQUIRED):
+      case this.checkErrorAndSetMessage('email', 'email', EMAIL_ERROR):
+      case this.checkErrorAndSetMessage('password', 'required', PASSWORD_REQUIRED):
+      case this.checkErrorAndSetMessage('password', 'minlength', PASSWORD_ERROR):
+      case this.checkErrorAndSetMessage('password', 'maxlength', PASSWORD_ERROR):
+      case this.checkErrorAndSetMessage('confirmPassword', 'required', CONFIRM_PASSWORD_REQUIRED):
+      case this.checkErrorAndSetMessage('confirmPassword', 'matching', CONFIRM_PASSWORD_ERROR):
+      case this.checkErrorAndSetMessage('recaptcha', 'required', RECAPTCHA_ERROR):
+      case this.checkErrorAndSetMessage('agreedToTerms', 'required', TERMS_ERROR):
+        return;
+      default:
+        this.checkRegisterUser(name, email, password /*recaptcha*/);
+        break;
     }
+  }
+
+  checkErrorAndSetMessage(controlName: string, errorType: string, errorMessage: string): boolean {
+    const control = this.registrationForm.get(controlName);
+
+    if (control?.hasError(errorType)) {
+      this.error = errorMessage;
+      return true;
+    }
+
+    return false;
   }
 
   checkRegisterUser(name: string, email: string, password: string /*recaptcha: string*/): void {
     this.profileService.existingUser(email).subscribe((data) => {
       if (data) {
-        this.error = 'Este email já está registrado.';
+        this.error = this.ERRORS.EMAIL_ALREADY_REGISTERED;
       } else {
         this.handleRegister(name, email, password /*recaptcha*/);
       }
@@ -77,7 +123,7 @@ export class RegisterComponent {
         this.navigateTo('login');
       },
       error: () => {
-        this.error = 'Erro no registro.';
+        this.error = this.ERRORS.REGISTRATION_ERROR;
 
         if (this.captchaElem) {
           this.captchaElem.resetCaptcha();
