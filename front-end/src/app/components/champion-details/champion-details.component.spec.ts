678c6c94e89a2b8c13b328957a49a1ead6457ce2
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-12 15:21:07 -0300

    test(components): champion-details in the frontend

--- front-end/src/app/components/champion-details/champion-details.component.spec.ts
+++ front-end/src/app/components/champion-details/champion-details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ChampionDetailsComponent } from './champion-details.component';
 
@@ -8,10 +10,9 @@ describe('ChampionDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChampionDetailsComponent]
-    })
-    .compileComponents();
-    
+      imports: [ChampionDetailsComponent, HttpClientModule, RouterTestingModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ChampionDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +21,16 @@ describe('ChampionDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should render the loading message correctly', () => {});
+
+  // it('should render the champion details correctly - name, title, tags, image, lore', () => {});
+
+  // it('should render the champion details correctly - skills', () => {});
+
+  // it('should render the champion details correctly - skins', () => {});
+
+  // it('should toggle skill on button click', () => {});
+
+  // it('should toggle skin on button click', () => {});
 });
