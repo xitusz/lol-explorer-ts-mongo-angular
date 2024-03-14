765109ded1f58336bd2fc5aa01dba2b4f14cbc8f
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-14 10:34:06 -0300

    test(services): favorite in the frontend

--- front-end/src/app/services/favorite.service.spec.ts
+++ front-end/src/app/services/favorite.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FavoriteService } from './favorite.service';
 
@@ -6,11 +7,23 @@ describe('FavoriteService', () => {
   let service: FavoriteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(FavoriteService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // it('create', () => {});
+
+  // it('getFavorites', () => {});
+
+  // it('addFavorite', () => {});
+
+  // it('deleteFavorite', () => {});
+
+  // it('clearFavorites', () => {});
 });
