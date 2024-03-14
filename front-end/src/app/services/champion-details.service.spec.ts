7ef515e66623c5c932ab814b6a28b36a83c5a374
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-14 10:33:50 -0300

    test(services): champion-details in the frontend

--- front-end/src/app/services/champion-details.service.spec.ts
+++ front-end/src/app/services/champion-details.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ChampionDetailsService } from './champion-details.service';
 
@@ -6,11 +7,17 @@ describe('ChampionDetailsService', () => {
   let service: ChampionDetailsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ChampionDetailsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // it('getChampionDetails', () => {});
+
+  // it('getSkillVideos', () => {});
 });
