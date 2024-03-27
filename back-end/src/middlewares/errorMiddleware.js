3620e37a8afab4f9635267f93923ae72db11e87e
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-27 13:53:40 -0300

    chore(): remove backend

--- back-end/src/middlewares/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const errorMiddleware = (err, _req, res, _next) => {
-  res.status(err.statusCode).json({ message: err.message });
-};
-
-module.exports = errorMiddleware;
