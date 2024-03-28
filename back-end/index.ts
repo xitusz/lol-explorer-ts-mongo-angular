7174f48747e7cb55af8257985d55199c85d7c2c2
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-28 01:48:46 -0300

    feat(): index in the backend

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
-import db from "./src/database/models";
+import cors from "cors";
+import routes from "./src/routes";
+import errorMiddleware from "./src/middlewares/errorMiddleware";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
-db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Api rodando na porta ${port}`);
-  });
-});
+app.use(routes);
+
+app.use(errorMiddleware);
+
+app.listen(port);
+
+console.log(`Api rodando na porta ${port}`);
 
 export default app;
