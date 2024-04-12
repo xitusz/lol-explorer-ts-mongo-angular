8827426622517cc91ddb0bf2b5d1a4e96a8d6b30
G-Alves1 <2kgabrielalves@gmail.com>
2024-04-12 11:41:13 -0300

    test(components): when user is not loggedin in header in the frontend

--- front-end/src/app/components/header/header.component.spec.ts
+++ front-end/src/app/components/header/header.component.spec.ts
@@ -1,33 +1,82 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent, RouterTestingModule],
+      imports: [HeaderComponent, RouterTestingModule, HttpClientModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  /*describe('when user is not logged in', () => {
-    it('should render login and register buttons', () => {});
+  describe('when user is not logged in', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
 
-    it('should redirect to login page when login button is clicked', () => {});
+    it('should render login and register buttons', () => {
+      fixture.detectChanges();
 
-    it('should redirect to register page when register button is clicked', () => {});
-  });*/
+      const loginButton = fixture.nativeElement.querySelector('.login-button');
+      const registerButton = fixture.nativeElement.querySelector('.register-button');
+
+      expect(loginButton).toBeTruthy();
+      expect(registerButton).toBeTruthy();
+    });
+
+    it('should redirect to login page when login button is clicked', () => {
+      spyOn(router, 'navigateByUrl');
+
+      fixture.detectChanges();
+
+      const loginButton = fixture.nativeElement.querySelector('.login-button');
+
+      loginButton.click();
+
+      fixture.detectChanges();
+
+      expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/login'), {
+        skipLocationChange: false,
+        replaceUrl: false,
+        state: undefined,
+        info: undefined,
+      });
+    });
+
+    it('should redirect to register page when register button is clicked', () => {
+      spyOn(router, 'navigateByUrl');
+
+      fixture.detectChanges();
+
+      const registerButton = fixture.nativeElement.querySelector('.register-button');
+
+      registerButton.click();
+
+      fixture.detectChanges();
+
+      expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/register'), {
+        skipLocationChange: false,
+        replaceUrl: false,
+        state: undefined,
+        info: undefined,
+      });
+    });
+  });
 
   /*describe('when user is logged in', () => {
     it('should render profile and exit buttons', () => {});
