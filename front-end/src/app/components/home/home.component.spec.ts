e43d5deed824f2cd63531f978d0f13167e8edf5a
G-Alves1 <2kgabrielalves@gmail.com>
2024-04-12 12:26:19 -0300

    test(components): home in the frontend

--- front-end/src/app/components/home/home.component.spec.ts
+++ front-end/src/app/components/home/home.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,22 +16,102 @@ describe('HomeComponent', () => {
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should render the champion', () => {});
+  it('should render the champion', () => {
+    const championImg = fixture.nativeElement.querySelector('[data-testid="champion-img"]');
+    const championHeading = fixture.nativeElement.querySelector('[data-testid="champion-heading"]');
 
-  // it('should render the region', () => {});
+    expect(championImg).toBeTruthy();
+    expect(championHeading).toBeTruthy();
+  });
 
-  // it('should redirect to champion page when image is clicked', () => {});
+  it('should render the region', () => {
+    const regionImg = fixture.nativeElement.querySelector('[data-testid="region-img"]');
+    const regionHeading = fixture.nativeElement.querySelector('[data-testid="region-heading"]');
 
-  // it('should redirect to region page when image is clicked', () => {});
+    expect(regionImg).toBeTruthy();
+    expect(regionHeading).toBeTruthy();
+  });
 
-  // it('should redirect to champion page when heading is clicked', () => {});
+  it('should redirect to champion page when image is clicked', () => {
+    spyOn(router, 'navigateByUrl');
 
-  // it('should redirect to region page when heading is clicked', () => {});
+    fixture.detectChanges();
+
+    const championImg = fixture.nativeElement.querySelector('[data-testid="champion-img"]');
+
+    championImg.click();
+
+    fixture.detectChanges();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/champion'), {
+      skipLocationChange: false,
+      replaceUrl: false,
+      state: undefined,
+      info: undefined,
+    });
+  });
+
+  it('should redirect to region page when image is clicked', () => {
+    spyOn(router, 'navigateByUrl');
+
+    fixture.detectChanges();
+
+    const regionImg = fixture.nativeElement.querySelector('[data-testid="region-img"]');
+
+    regionImg.click();
+
+    fixture.detectChanges();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/region'), {
+      skipLocationChange: false,
+      replaceUrl: false,
+      state: undefined,
+      info: undefined,
+    });
+  });
+
+  it('should redirect to champion page when heading is clicked', () => {
+    spyOn(router, 'navigateByUrl');
+
+    fixture.detectChanges();
+
+    const championHeading = fixture.nativeElement.querySelector('[data-testid="champion-heading"]');
+
+    championHeading.click();
+
+    fixture.detectChanges();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/champion'), {
+      skipLocationChange: false,
+      replaceUrl: false,
+      state: undefined,
+      info: undefined,
+    });
+  });
+
+  it('should redirect to region page when heading is clicked', () => {
+    spyOn(router, 'navigateByUrl');
+
+    fixture.detectChanges();
+
+    const regionHeading = fixture.nativeElement.querySelector('[data-testid="region-heading"]');
+
+    regionHeading.click();
+
+    fixture.detectChanges();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith(jasmine.stringMatching('/region'), {
+      skipLocationChange: false,
+      replaceUrl: false,
+      state: undefined,
+      info: undefined,
+    });
+  });
 });
