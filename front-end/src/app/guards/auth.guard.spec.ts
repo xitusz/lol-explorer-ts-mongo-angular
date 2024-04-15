16108a14ed24c4dae237c175fd85d7bb23f83417
G-Alves1 <2kgabrielalves@gmail.com>
2024-04-15 18:19:42 -0300

    test(guards): auth in the frontend

--- front-end/src/app/guards/auth.guard.spec.ts
+++ front-end/src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 
 import { authGuard } from './auth.guard';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) => TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow access when user is authenticated', () => {
+    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem('token', 'token');
+
+    const canActivate = executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+
+    expect(canActivate).toBeTruthy();
+  });
+
+  it('should redirect to home when user is not authenticated', () => {
+    localStorage.clear();
+    spyOn(router, 'navigateByUrl');
+
+    const canActivate = executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+
+    expect(canActivate).toBeFalsy();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('');
   });
 });
